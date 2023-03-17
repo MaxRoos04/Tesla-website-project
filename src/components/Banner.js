ea25b36a374d855020ead25af182a9fc2bd8ec17
keke rosberg <maxroos931@gmail.com>
2023-03-18 01:34:26 +0200

    Frontend stuff

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import Image from '../assets/front-page.png';
-import {TypeAnimation} from 'react-type-animation'; 
-import {motion} from 'framer-motion'; 
-import {fadeIn} from '../variants'; 
-import { Link } from 'react-scroll'
 
 const Banner = () => {
   return(
   <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
      <div className='container mx-auto'>
-      <div className='bg-Banner bg-contain bg-no-repeat w-6/6 h-[640px] ' style={{}}>
-     
-      <div className='flex-1'>
-          <h2 className='h2 text-accent mb-6' >TESLA</h2>
-          <h3 className='h3 max-w-[455px] mb-16' >Accelerating the world's transition to sustainable energy.</h3>
-          <Link 
-            to='work' 
-            activeClass='active'
-            smooth={true}
-            spy={true}
-            offset={-200}>
-         </Link>
+     <div className='bg-Banner flex-1 h-[600px] text-center'>
+          <h2 className='h2 text-gradient mb-6'>TESLA</h2>
+          <h3 className='h3  mb-16 ' >Accelerating the world's transition to sustainable energy.</h3>
         </div>
+      <div className=''>
         </div>
       </div>
    </section>
