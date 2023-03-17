94b3d2daa1fa4abacb5ff11620e08dbc41a4767b
keke rosberg <maxroos931@gmail.com>
2023-03-18 01:45:51 +0200

    Home-page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,9 +5,11 @@ const Banner = () => {
   return(
   <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
      <div className='container mx-auto'>
-     <div className='bg-Banner flex-1 h-[600px] text-center'>
+     <div className='bg-Banner lg:bg-center flex-1 h-[600px] text-center '>
+          <div className='align-middle'>
           <h2 className='h2 text-gradient mb-6'>TESLA</h2>
           <h3 className='h3  mb-16 ' >Accelerating the world's transition to sustainable energy.</h3>
+           </div>
         </div>
       <div className=''>
         </div>
