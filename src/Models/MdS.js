219dbb7ef0ec3e330b9d26dc0c5b0f3a77b0a7f5
keke rosberg <maxroos931@gmail.com>
2023-03-30 23:53:50 +0300

    Added pitch for MdS

--- src/Models/MdS.js
+++ src/Models/MdS.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Stats from '../assets/MdS-stats.png'
 
 const mdS = () => {
   return <section className='min-h-[85vh] min-w-[200vh] lg:min-h-[78vh] flex items-center' id='page1'>
@@ -7,6 +6,9 @@ const mdS = () => {
         <div className='font-primary'>
          <h1>Tesla Model S</h1>
          </div>
+         <div className='font-secondary'>
+          <h3>With its sleek, sporty design and premium interior, the Model S is the<br></br> ultimate luxury car for the eco-conscious driver.<br></br> And with its all-electric powertrain, you'll enjoy smooth,<br></br> silent acceleration and a range of up to 412 miles on a single charge.</h3>
+        </div>
       </div>
   </section>
 };
