3f1989ade10713ac4793c9c46cb3e532f01a22d7
keke rosberg <maxroos931@gmail.com>
2023-03-30 23:30:34 +0300

    Styling fp more

--- src/Models/Md3.js
+++ src/Models/Md3.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const mdS = () => {
-  return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='page2'>
+  return <section className='min-h-[85vh] min-w-[200vh] lg:min-h-[78vh] flex items-center' id='page2'>
       <div className='bg-MD3 lg:bg-right flex-1 h-[600px] text-left bg-no-repeat'>
         <div className='font-primary'>
          <h1>Tesla Model 3</h1>
--- src/Models/MdS.js
+++ src/Models/MdS.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Stats from '../assets/MdS-stats.png'
 
 const mdS = () => {
-  return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='page1'>
-      <div classname="bg">
-        <div classname="">
-          <h1></h1>
-        </div>
+  return <section className='min-h-[85vh] min-w-[200vh] lg:min-h-[78vh] flex items-center' id='page1'>
+      <div className='bg-MDS lg:bg-right flex-1 h-[600px] text-left bg-no-repeat'>
+        <div className='font-primary'>
+         <h1>Tesla Model S</h1>
+         </div>
       </div>
   </section>
 };
