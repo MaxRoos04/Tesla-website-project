7fe31fc98f8df0e32a53ef6b503c7bd45fc9f59b
keke rosberg <maxroos931@gmail.com>
2023-03-30 23:47:22 +0300

    Added pitch for md3

--- src/Models/Md3.js
+++ src/Models/Md3.js
@@ -4,8 +4,11 @@ const md3 = () => {
   return <section className='min-h-[85vh] min-w-[200vh] lg:min-h-[78vh] flex items-center' id='page2'>
       <div className='bg-MD3 lg:bg-right flex-1 h-[600px] text-left bg-no-repeat'>
         <div className='font-primary'>
-         <h1>Tesla Model 3</h1>
+          <h1>Tesla Model 3</h1>
          </div>
+        <div className='font-secondary'>¨
+          <h3> <h3>With its sleek, aerodynamic design, the Model 3 is a head-turner on the road.<br></br> But it's not just about looks: this car is designed to be as energy-efficient as possible, <br></br>with an impressive range of up to 353 miles on a single charge.<br></br> And with Tesla's extensive network of charging stations,<br></br> you'll never be far from a top-up.</h3></h3>
+        </div>
       </div>
  </section>
 };
