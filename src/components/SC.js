1d9929fbb25fc8f2fd2aef7f0e8d9094f5d8cde1
keke rosberg <maxroos931@gmail.com>
2023-04-20 23:21:17 +0300

    Remade the sc page!

--- src/components/SC.js
+++ src/components/SC.js
@@ -1,54 +1,56 @@
-import React from 'react';
-import CountUp from 'react-countup';
-import {useInView} from 'react-intersection-observer'
-import {motion} from 'framer-motion'
-import {fadeIn} from '../variants'
-import { Link } from 'react-scroll'
-import Image from '../assets/SC.png'
+import React from "react";
 
-const SC = () => {
-  const [ref, inView] = useInView({
-    threshold: 0.5,
-  }); 
-
+const SuperchargersSection = () =>{
   return (
-    <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
-        
-        <motion.div 
-        id = ''
-        variants={fadeIn('left', 0.5)}
-        initial='hidden'
-        whileInView={'show'}
-        viewport={{once: false, amount: 0.3}}
-        className='flex-1'>
-          <h2 className='h2'>Tesla Superchargers in Finland</h2>
-          <h3 className='h4 mb-4 text-gray font-secondary unbold'>Supercharger charging stations are located on major routes near services and keep your battery charged while you're on the go.</h3>
-          <p className='mb-6'></p>
-
-        <div className='flex gap-x-8 items-center'>
-        <Link 
-           to='services'
-           activeClass='active'
-           smooth={true}
-           spy={true}>
-           <button className='btn btn-lg'>Locations</button>
-        </Link>
-          <a href='#' className='text-gradient btn-link'>
-            Tesla.fi
-          </a>
+    <div className="flex flex-col items-center justify-center bg-gray-100 py-20 container mx-auto mt-60 ">
+      <div className="max-w-4xl text-center">
+        <h2 className="text-3xl font-bold">Unleash Your Adventures</h2>
+        <p className="text-lg text-gray-500 mt-4">
+          With Tesla's Supercharger network, you can travel farther and explore
+          more without worrying about running out of charge. Our Superchargers
+          are strategically placed along the most popular routes, allowing you
+          to easily charge up and continue your journey. And with the latest V3
+          Superchargers, you can charge up to 75 miles in just 5 minutes.
+        </p>
+        <div className="mt-10 flex flex-col sm:flex-row items-center justify-center">
+          <img
+            src="https://tesla-cdn.thron.com/delivery/public/image/tesla/3dec2bc6-a3f4-4d27-8249-05e78bbfa7d0/bvlatuR/std/2880x1620/Supercharger-Savings-Hero-Desktop-NA"
+            alt="Tesla Superchargers"
+            className="w-64 h-64 object-cover mr-4 mb-4 sm:mb-0"
+          />
+          <div className="text-left">
+            <h3 className="text-xl font-bold mb-2">Fast and Convenient</h3>
+            <p className="text-gray-500">
+              Tesla Superchargers are designed to get you back on the road as
+              quickly as possible. With an average charging time of 30-45
+              minutes, you can enjoy a meal, take a restroom break, or stretch
+              your legs while your car charges up. And with the latest V3
+              Superchargers, you can reduce your charging time even further.
+            </p>
+          </div>
         </div>
-       </motion.div>
-
-       <motion.div 
-        variants={fadeIn('right', 0.3)}
-        initial='hidden'
-        whileInView={'show'}
-        viewport={{once: false, amount: 0.3}}
-        className='flex-1 bg-SC bg-no-repeat h-[640px] ' style={{marginRight: "10%"}}>
-          <img src={Image} alt=''/>
-        </motion.div>
+        <div className="mt-10 flex flex-col sm:flex-row items-center justify-center">
+          <div className="text-left">
+            <h3 className="text-xl font-bold mb-2">A Better Future</h3>
+            <p className="text-gray-500">
+              By using Tesla Superchargers, you're not just getting a convenient
+              charging solution. You're also helping to accelerate the world's
+              transition to sustainable energy. Tesla Superchargers are powered
+              by renewable energy sources like solar and wind power, which
+              helps to reduce our dependence on fossil fuels and decrease
+              carbon emissions. So every time you charge up, you're making a
+              positive impact on the environment.
+            </p>
+          </div>
+          <img
+            src="https://www.notateslaapp.com/images/news/2021/superchargers-wifi.jpg"
+            alt="Tesla Supercharger"
+            className="w-64 h-64 object-cover ml-4"
+          />
+        </div>
       </div>
-  )
+    </div>
+  );
 };
 
-export default SC;
+export default SuperchargersSection;
